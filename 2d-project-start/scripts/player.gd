7f5328c46809474b192880bad68e66479a80f1a6
Daniel <81522135+Glaciian@users.noreply.github.com>
2025-03-19 08:47:21 +1100

    added movement

--- 2d-project-start/scripts/player.gd
+++ 2d-project-start/scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @export var speed = 100
+@onready var player = %HappyBoo
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -8,6 +9,6 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 	if velocity.length() > 0.0:
-		get_node("HappyBoo").play_walk_animation()
+		player.play_walk_animation()
 	else:
-		get_node("HappyBoo").play_idle_animation()
+		player.play_idle_animation()
